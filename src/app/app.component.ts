556c0568d6c6718095717e5450730f80b4caee33
olcayhank <kolcayhan1@gmail.com>
2023-07-03 19:36:11 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
 import { Component } from '@angular/core';
+import { History } from 'src/types';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'calculator';
+  
+  historyClicked: boolean = false;
+  storedValue: string | null = localStorage.getItem('mode');
+
+  isActive: boolean = this.storedValue ? JSON.parse(this.storedValue) : false;
+  result: string = '';
+  value: string = '';
+  calculated: boolean = false;
+  history: History[] = [];
+
+  toggleScreen(): void {
+    this.isActive = !this.isActive;
+    localStorage.setItem('mode', String(this.isActive));
+  }
+
+  loadCalculate(data: string): void {
+    if (this.calculated) {
+      this.clearFunc();
+      this.calculated = false;
+    }
+
+    if (
+      !Number(this.value[this.value.length - 1]) &&
+      (data === '/' || data === '*' || data == '-' || data == '+')
+    )
+      return;
+    this.value = this.value + data;
+  }
+
+  calculateFunc(): void {
+    if (
+      this.result === undefined ||
+      !Number(this.value[this.value.length - 1])
+    ) {
+      this.result = 'Error';
+      this.value = '';
+      return;
+    }
+    this.result = eval(this.value);
+    this.setHistory(this.value, this.result);
+  }
+
+  setHistory(value: string, result: string) {
+    this.calculated = true;
+    this.history.push({ value, result });
+    if (this.history.length > 3) this.history = this.history.slice(1);
+  }
+
+  getHistory() {
+    this.historyClicked = !this.historyClicked;
+  }
+
+  calculatePercentage() {
+    this.result = String(eval(this.value) / 100);
+    this.setHistory(this.value, this.result);
+  }
+
+  changePrefix() {
+    if (Number(this.value) && Number(this.value) > 0) {
+      this.value = '-' + this.value;
+    } else if (Number(this.value) < 0) {
+      this.value = this.value.slice(1);
+    }
+  }
+
+  clearFunc() {
+    this.value = '';
+    this.result = '';
+  }
 }
